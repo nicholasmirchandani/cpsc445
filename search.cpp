3d7ba588417ead9c5fa6e1bea8cf8f1eeb12e0ff
nicholasmirchandani <nmirchandani@chapman.edu>
2021-10-09 22:23:15 +0000

    Adds arg parsing

--- search.cpp
+++ search.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        std::cout << "Usage: " << argv[0] << " <keywords-file> <text-file> <output-file>" << std::endl;
+        exit(1);
+    }
+
+    std::cout << "Keywords file: " << argv[1] << std::endl;
+    std::cout << "Text file: " << argv[2] << std::endl;
+    std::cout << "Output file: " << argv[3] << std::endl;
 }
