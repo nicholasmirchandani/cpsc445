7bae8b4d981c1c6e4d3ae11a1beff65d3c101c46
nicholasmirchandani <nmirchandani@chapman.edu>
2021-10-27 06:30:10 +0000

    Flips order for CI

--- dna_parse.cpp
+++ dna_parse.cpp
@@ -136,7 +136,7 @@ int main (int argc, char *argv[]) {
     for(int i = 0; i < 4; ++i) {
         for(int j = 0; j < 4; ++j) {
             for(int k = 0; k < 4; ++k) {
-                os << returnNucleotide(k) << returnNucleotide(j) << returnNucleotide(i) << " " << counts[k + 4 * j + 16 * i] << std::endl;
+                os << returnNucleotide(i) << returnNucleotide(j) << returnNucleotide(k) << " " << counts[i + 4 * j + 16 * k] << std::endl;
             }
         }
     }
