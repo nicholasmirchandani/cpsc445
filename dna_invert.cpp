9db7a6f6dc12b36d2e8a88fa4323e53f6ddeb24f
nicholasmirchandani <nmirchandani@chapman.edu>
2021-10-27 06:05:49 +0000

    dna_invert take 1

--- dna_invert.cpp
+++ dna_invert.cpp
@@ -27,8 +27,9 @@ int main (int argc, char *argv[]) {
 
   // example code
   char n[MAX_BUF];
+  char n_final[MAX_BUF];
   char* recv_buf = new char[MAX_BUF / p];
-  int numCharsToSend;
+  int numCharsToSend = 0;
   if(rank == 0) {
       std::ifstream is("dna.txt");
       if (is.fail()) {
@@ -52,17 +53,52 @@ int main (int argc, char *argv[]) {
       is.close();
 
       strcpy(n, allChars.c_str());
-      numCharsToSend = allChars.length();
+      for(int i = strlen(n); i < MAX_BUF; ++i) {
+      // Ensures 0s are padded after string as needed
+        n[i] = '\0';
+      }
+      numCharsToSend = (allChars.length() / p) + ((strlen(n) % p) == 0 ? 0 : 1);
+      for(int i = 1; i < p; ++i) {
+        MPI_Send(&numCharsToSend, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+      }
+  } else {
+    MPI_Status status;
+    MPI_Recv(&numCharsToSend, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
   }
-  // TODO: Scatter chunks of the string instead of an int
+  // Scatter chunks of the string
   check_error(MPI_Scatter(n, numCharsToSend, MPI_CHAR, recv_buf, numCharsToSend, MPI_CHAR, 0, MPI_COMM_WORLD));
 
   // TODO: Flip everything within the recv_buf
+  for(int i = 0; i < numCharsToSend; ++i) {
+    if(recv_buf[i] == '\0') {
+      // Break out of loop on null terminating character, since we don't know the length of the string
+      break;
+    }
+    switch(recv_buf[i]) {
+      case 'A':
+        recv_buf[i] = 'T';
+        break;
+      case 'C':
+        recv_buf[i] = 'G';
+        break;
+      case 'T':
+        recv_buf[i] = 'A';
+        break;
+      case 'G':
+        recv_buf[i] = 'C';
+        break;
+      default:
+        std::cout << "Unknown character detected. Character is ' " << recv_buf[i] << " ' Skipping" << std::endl;
+        break;
+    }
+  }
+
+  // TODO: Gather everything within the recv bufs and output
+  check_error(MPI_Gather(recv_buf, numCharsToSend, MPI_CHAR, n_final, numCharsToSend, MPI_CHAR, 0, MPI_COMM_WORLD));
 
   delete(recv_buf);
 
-  // TODO: Gather everything from recv_bufs and output
-
+  // Reduce counts of As, Cs, Ts, and Gs into process of rank 0, independently as 4 separate reduce calls
   if (rank==0) {
     ofstream os("output.txt");
 
@@ -70,7 +106,7 @@ int main (int argc, char *argv[]) {
         std::cout << "Unable to open output file.  Exiting " << std::endl;
     }
 
-    os << "Some Output" << std::endl;
+    os << n_final;
 
     os.close();
   }
