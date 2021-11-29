032270113d2c3d1ff71e0afd75129c88a3b721a7
nicholasmirchandani <nmirchandani@chapman.edu>
2021-11-28 16:37:21 -0800

    Actual attempt of dna_genes

--- dna_genes.cu
+++ dna_genes.cu
@@ -5,36 +5,9 @@
 
 #define MAX_BUF 10000
 
-char returnNucleotide(int num) {
-    switch(num) {
-        case 0:
-            return 'A';
-        case 1:
-            return 'C';
-        case 2:
-            return 'G';
-        case 3:
-            return 'T';
-        default:
-            return '?';
-    }
-}
-
-__global__ void count_step_1(char* dn, int* dsums, int numChars) {
+__global__ void genes_calc(char* dn, int* dstates, int numChars) {
     int shift = gridDim.x * blockDim.x;
     int offset = blockIdx.x * blockDim.x + threadIdx.x;
-
-    for(int i = 0; i < 64; ++i) {
-        dsums[i + 64 * offset] = 0;
-    }
-    
-    // Count every trigram
-    const int A_KEY = 0;
-    const int C_KEY = 1;
-    const int G_KEY = 2;
-    const int T_KEY = 3;
-    
-    int curKeys[3];
 
     numChars = (numChars / shift) + ((numChars % shift) == 0 ? 0 : 1);
     while(numChars % 3 != 0) {
@@ -42,65 +15,23 @@ __global__ void count_step_1(char* dn, int* dsums, int numChars) {
     }
     offset *= numChars;
 
-    for(int i = offset; i < numChars + offset; i += 3) {
-        curKeys[0] = 0;
-        curKeys[1] = 1;
-        curKeys[2] = 2;
-        int j;
-        bool stopNow = false;
-        for(j = i; j < i + 3; ++j) {
-            switch(dn[j]) {
-                case 'A':
-                    curKeys[j-i] = A_KEY;
-                    break;
-                case 'C':
-                    curKeys[j-i] = C_KEY;
-                    break;
-                case 'T':
-                    curKeys[j-i] = T_KEY;
-                    break;
-                case 'G':
-                    curKeys[j-i] = G_KEY;
-                    break;
-                default:
-                    stopNow = true;
-                    break;
-            }
+    for(int i = offset; i + 2 < numChars + offset; i += 3) {
+        int index = i/3;
+        if(dn[i] == 'A' && dn[i+1] == 'T' && dn[i+2] == 'G') {
+            dstates[index] = 1;
         }
 
-        if(stopNow) {
-            break;
+        if(dn[i] == 'T' && dn[i+1] == 'A' && dn[i+2] == 'G') {
+            dstates[index] = 2;
         }
-        int targetIndex = curKeys[2] * 16 + curKeys[1] * 4 + curKeys[0];
-        dsums[targetIndex + 64 * offset] += 1;
-    }
 
-    __syncthreads();
-    __shared__ int tmpsums[4*64]; // 4*64 for 4 threads per block
-
-    for(int i = 0; i < 64; ++i) {
-        tmpsums[i + 64 * threadIdx.x] = dsums[i + 64 * offset];
-    }
-
-    __syncthreads();
-
-    for(int delta = 1; delta < blockDim.x; delta *= 2) {
-        if((threadIdx.x + delta) < blockDim.x) {
-            for(int i = 0; i < 64; ++i) {
-                tmpsums[i + 64 * threadIdx.x] = tmpsums[i + 64 * threadIdx.x] + tmpsums[i + 64 * (delta + threadIdx.x)];
-            }
+        if(dn[i] == 'T' && dn[i+1] == 'A' && dn[i+2] == 'A') {
+            dstates[index] = 2;
         }
-        __syncthreads();
-    }
 
-    __syncthreads();
-
-    // Combine all threads within the same block; Currently issue with shared memory not being used properly
-
-    offset = blockDim.x * blockIdx.x;
-
-    for(int i = 0; i < 64; ++i) {
-        dsums[i + 64 * offset] = tmpsums[i];
+        if(dn[i] == 'T' && dn[i+1] == 'G' && dn[i+2] == 'A') {
+            dstates[index] = 2;
+        }
     }
 }
 
@@ -134,9 +65,9 @@ __global__ void count_step_2(int* dsums, int W) {
 int main() {
 
     char* n = new char[MAX_BUF];
-    int* sums = new int[64];
-    for(int i = 0; i < 64; ++i) {
-        sums[i] = 0;
+    int* states = new int[MAX_BUF];
+    for(int i = 0; i < MAX_BUF; ++i) {
+        states[i] = 0;
     }
 
     std::ifstream is("dna.txt");
@@ -149,14 +80,14 @@ int main() {
     std::string allChars = "";
     std::string line;
     while(is.good()) {
-    std::getline(is, line);
+        std::getline(is, line);
 
-    // Remove trailing newline and carriage returns
-    while(line[line.length()-1] == '\n' || line[line.length()-1] == '\r') {
-        line.pop_back();
-    }
+        // Remove trailing newline and carriage returns
+        while(line[line.length()-1] == '\n' || line[line.length()-1] == '\r') {
+            line.pop_back();
+        }
 
-    allChars.append(line);
+        allChars.append(line);
     }
 
     is.close();
@@ -168,44 +99,43 @@ int main() {
     }
 
     char* dn;
-    int* dsums;
+    int* dstates;
     int numBlocks = 4;
     int numThreads = 4;
     cudaMalloc((void**) &dn, MAX_BUF * sizeof(char));
-    cudaMalloc((void**) &dsums, 64 * numThreads * numBlocks * sizeof(int));
+    cudaMalloc((void**) &dstates, MAX_BUF * numThreads * numBlocks * sizeof(int));
     cudaMemcpy(dn, n, MAX_BUF * sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemcpy(dsums, sums, 64 * numThreads * numBlocks * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dstates, states, MAX_BUF * numThreads * numBlocks * sizeof(int), cudaMemcpyHostToDevice);
 
-    std::cout << "Step 1!" << std::endl;
-    count_step_1<<<numBlocks, numThreads>>>(dn, dsums, strlen(n));
+    genes_calc<<<numBlocks, numThreads>>>(dn, dstates, strlen(n));
     cudaDeviceSynchronize();
 
-    std::cout << "Step 2!" << std::endl;
-    count_step_2<<<1, numBlocks>>>(dsums, numThreads);
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(sums, dsums, 64 * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(states, dstates, MAX_BUF * sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(dn);
-    cudaFree(dsums);
+    cudaFree(dstates);
 
+    bool inGene = false;
+    int startIndex = -1;
     std::ofstream os("output.txt");
-
+    
     if(os.fail()) {
         std::cout << "Unable to open output file.  Exiting " << std::endl;
+        exit(1);
     }
 
-    for(int i = 0; i < 4; ++i) {
-        for(int j = 0; j < 4; ++j) {
-            for(int k = 0; k < 4; ++k) {
-                if(sums[i + 4 * j + 16 * k] != 0) {
-                    os << returnNucleotide(i) << returnNucleotide(j) << returnNucleotide(k) << " " << sums[i + 4 * j + 16 * k] << std::endl;
-                }
-            }
+    for(int i = 0; i < MAX_BUF; ++i) {
+        if(states[i] == 1 && !inGene) {
+            inGene = true;
+            startIndex = i;
+        } else if (states[i] == 2 && inGene) {
+            inGene = false;
+            os << startIndex << " " << i << std::endl;
         }
     }
 
+
     os.close();
     delete(n);
-    delete(sums);
+    delete(states);
     return 0;
 }
