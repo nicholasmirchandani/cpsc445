164429b2c327502a9d119f4dbe2f0a0ba6c46982
nicholasmirchandani <nmirchandani@chapman.edu>
2021-12-05 23:45:05 -0800

    Modifies extreme see what the error is

--- extreme.cu
+++ extreme.cu
@@ -5,7 +5,7 @@
 
 #define MAX_BUF 200000
 
-void readCSV(float* nums, int& numFloats, std::string filename) {
+void readCSV(float* nums, int& numFloats, int& floatsPerRow, std::string filename) {
     std::ifstream is(filename);
 
     if (is.fail()) {
@@ -17,6 +17,7 @@ void readCSV(float* nums, int& numFloats, std::string filename) {
     std::string line;
     while(is.good()) {
         std::getline(is, line);
+        std::cout << "Line read: " << line << std::endl;
 
         // Remove trailing newline, carriage returns
         while(line[line.length()-1] == '\n' || line[line.length()-1] == '\r' || line[line.length()-1] == ',') {
@@ -40,37 +41,64 @@ void readCSV(float* nums, int& numFloats, std::string filename) {
         if (element != "") {
             nums[numFloats++] = std::stof(element);
         }
+    
+        if(floatsPerRow == -1) {
+            floatsPerRow = numFloats;
+            std::cout << "Floats per row set!" << std::endl;
+        }
     }
+    std::cout << "is is not good :(" << std::endl;
 }
 
-__global__ void cuda_sqrt(float* dnums, int numFloats) {
+__global__ void find_extremes(float* dnums, int numFloats, int floatsPerRow, bool* disExtreme) {
     int shift = gridDim.x * blockDim.x;
     int offset = blockIdx.x * blockDim.x + threadIdx.x;
 
+    bool isMin = true;
+    bool isMax = true;
     for(int i = offset; i < numFloats; i += shift) {
-        dnums[i] = sqrt(dnums[i]);
+        // For 8 neighbors, where i - floatsPerRow - 1 is top left and i + floatsPerRow + 1 is bottom right
+        for(int j = -1; j < 2; ++j) {
+            for(int k = -1; k < 2; ++k) {
+                if(j == 0 && k == 0) {
+                    // Skip element in its own loop
+                    continue;
+                }
+
+                isMin = isMin && (dnums[i] < dnums[i + j * floatsPerRow + k * 1]); // Neighbor :)
+                isMax = isMax && (dnums[i] > dnums[i + j * floatsPerRow + k * 1]);
+            }
+        }
+        
+        disExtreme[i] = isMin || isMax;
     }
 }
 
 int main() {
+    std::cout << "MAIN!" << std::endl;
     float nums[MAX_BUF];
+    bool isExtreme[MAX_BUF];
     int numFloats = 0;
+    int floatsPerRow = -1;
 
     system("head input.csv");
 
-    readCSV(nums, numFloats, "input.csv");
+    readCSV(nums, numFloats, floatsPerRow, "input.csv");
 
     // Now we have the csv properly parsed, we do the parallel sqrt computation
     float* dnums;
+    bool* disExtreme;
     cudaMalloc((void**) &dnums, numFloats * sizeof(float));
     cudaMemcpy(dnums, nums, numFloats * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void**) &disExtreme, numFloats * sizeof(bool));
 
     int numBlocks = 2;
     int numThreads = 4;
 
-    cuda_sqrt<<<numBlocks, numThreads>>>(dnums, numFloats);
+    std::cout << "Find extremes!" << std::endl;
+    find_extremes<<<numBlocks, numThreads>>>(dnums, numFloats, floatsPerRow, disExtreme);
 
-    cudaMemcpy(nums, dnums, numFloats * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(isExtreme, disExtreme, numFloats * sizeof(bool), cudaMemcpyDeviceToHost);
 
     std::ofstream os("output.csv");
 
@@ -78,11 +106,15 @@ int main() {
         std::cout << "Unable to open output file.  Exiting " << std::endl;
     }
 
+
+    std::cout << "RESULTS: " << std::endl << std::endl;
     for(int i = 0; i < numFloats - 1; ++i) {
-        os << nums[i] << std::endl;
+        int rowNum = i / floatsPerRow;
+        int colNum = i % floatsPerRow;
+        if(isExtreme[i]) {
+            std::cout << "(" << colNum << "," << rowNum << ")" << std::endl;
+        }
     }
-    // Last element shouldn't have comma space after it
-    os << nums[numFloats-1];
 
     os.close();
 
