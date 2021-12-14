25dd2e7c4ccfe44b3043f28b4f119a5a6d89797b
nicholasmirchandani <nmirchandani@chapman.edu>
2021-12-13 23:32:53 -0800

    Adds working parallel implementation... but it's hella slow :(

--- main.cu
+++ main.cu
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <chrono>
 
-#define NUM_POLYGONS 1000
+#define NUM_POLYGONS 100
 
-bool pointInTri(float** p, float p1x, float p1y);
-bool checkTesselatedTris(float** p1, float p1_num_verts, float** p2, float p2_num_verts);
-bool checkOverlap(float** p1, float p1_num_verts, float** p2, float p2_num_verts);
+bool pointInTri(float* p, float p1x, float p1y);
+bool checkTesselatedTris(float* p1, float p1_num_verts, float* p2, float p2_num_verts);
+bool checkOverlap(float* p1, float p1_num_verts, float* p2, float p2_num_verts);
 
-__device__ bool pointInTri_device(float** p, float p1x, float p1y);
-__device__ bool checkTesselatedTris_device(float** p1, float p1_num_verts, float** p2, float p2_num_verts);
-__device__ bool checkOverlap_device(float** p1, float p1_num_verts, float** p2, float p2_num_verts);
+__device__ bool pointInTri_device(float* p, float p1x, float p1y);
+__device__ bool checkTesselatedTris_device(float* p1, float p1_num_verts, float* p2, float p2_num_verts);
+__device__ bool checkOverlap_device(float* p1, float p1_num_verts, float* p2, float p2_num_verts);
 
 
-__global__ void parallel_checkOverlap(float*** dps, int* dp_counts, int num_polys) {
+__global__ void parallel_checkOverlap(float* dps, int* dp_counts, int num_polys) {
     int shift = gridDim.x * blockDim.x;
     int offset = blockIdx.x * blockDim.x + threadIdx.x;
 
     for(int i = offset; i < num_polys; i += shift) {
         for(int j = i + 1; j < num_polys; ++j) {
-            bool result = checkOverlap_device(dps[i], dp_counts[i], dps[j], dp_counts[j]);
+            bool result = checkOverlap_device(&dps[i * 10 * 2], dp_counts[i], &dps[j * 10 * 2], dp_counts[j]);
         }
     }
 }
@@ -27,20 +27,16 @@ int main() {
     // TODO: Call delete and free and cudafree
     srand(time(0));
     // 3 Polygons
-    float*** polygons = new float**[NUM_POLYGONS];
+    float* polygons = new float[NUM_POLYGONS * 10 * 2];
     int* polygonCounts = new int[NUM_POLYGONS];
     for (int i = 0; i < NUM_POLYGONS; ++i) {
         // Assuming all polygons have 3-10 vertices, chosen at random
         int randNum = (rand() % 8) + 3;
         int numVertices = randNum;
         polygonCounts[i] = numVertices;
-        polygons[i] = new float*[numVertices];
         for(int j = 0; j < numVertices; ++j) { 
-            polygons[i][j] = new float[2];
-
-            // TODO: How to randomly generate points?
-            polygons[i][j][0] = rand()/(float) RAND_MAX;
-            polygons[i][j][1] = rand()/(float) RAND_MAX;
+            polygons[i * 10 * 2 + j * 2 + 0] = rand()/(float) RAND_MAX;
+            polygons[i * 10 * 2 + j * 2 + 1] = rand()/(float) RAND_MAX;
         }
     }
 
@@ -49,7 +45,7 @@ int main() {
     // Iteration through triangles inspired by selection sort.  All pairs of triangles need to be checked for overlapping, so no further optimization can be made
     for(int i = 0; i < NUM_POLYGONS; ++i) {
         for(int j = i + 1; j < NUM_POLYGONS; ++j) {
-            bool result =  checkOverlap(polygons[i], polygonCounts[i], polygons[j], polygonCounts[j]);
+            bool result = checkOverlap(&polygons[i * 10 * 2], polygonCounts[i], &polygons[j * 10 * 2], polygonCounts[j]);
             // std::cout << "RESULT: " << result << std::endl;
         }
     }
@@ -59,35 +55,39 @@ int main() {
 
     start = std::chrono::steady_clock::now();
     // TODO: Parallel implementation
+    
     float* dps;
     int* dp_counts;
     cudaMalloc((void**)&dps, NUM_POLYGONS * 10 * 2 * sizeof(float)); // Since max Vertices is 10 we're allocating that :)
     //dps[polyNum * 10 * 2 + vertexNum * 2 + offset(0/1) ]
     cudaMalloc((void**)&dp_counts, NUM_POLYGONS * sizeof(int));
 
-    // cudaMemcpy(dps111, sums, 4 * numThreads * numBlocks * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dps, polygons, NUM_POLYGONS * 10 * 2 * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dp_counts, polygonCounts, NUM_POLYGONS * sizeof(int), cudaMemcpyHostToDevice);
 
-
-
+    int numBlocks = 1;
+    int numThreads = 1;
+    parallel_checkOverlap<<<numBlocks, numThreads>>>(dps, dp_counts, NUM_POLYGONS);
+    cudaDeviceSynchronize();
     end = std::chrono::steady_clock::now();
     diff = end - start;
     std::cout << "Time to complete parallel implementation: " <<  diff.count() << "\n";
 
 }
 
-bool pointInTri(float** p, float p1x, float p1y) {
+bool pointInTri(float* p, float p1x, float p1y) {
     bool inTriangle = true;
     
     // If point not within triangle, then 3 eqs bounding triangle will not be true, so inTriangle will be false
     for(int i = 0; i < 3; ++i) {
-        if (p[(i+1) % 3][0] - p[i][0] == 0) {
-            bool isRight = p[(i+2) % 3][0] > p[i][0];
-            inTriangle = inTriangle && (isRight ? p1x >= p[i][0] : p1x <= p[i][0]); 
+        if (p[((i+1) % 3) * 2 + 0] - p[i * 2 + 0] == 0) {
+            bool isRight = p[((i+2) % 3 * 2) + 0] > p[i * 2 + 0];
+            inTriangle = inTriangle && (isRight ? p1x >= p[i * 2 + 0] : p1x <= p[i * 2 + 0]); 
         } else {
-            float m = (p[(i+1) % 3][1] - p[i][1])/(p[(i+1) % 3][0] - p[i][0]);
-            float b = -m * p[i][0] + p[i][1];
+            float m = (p[((i+1) % 3) * 2 + 1] - p[i * 2 + 1])/(p[((i+1) % 3) * 2 + 0] - p[i * 2 + 0]);
+            float b = -m * p[i * 2 + 0] + p[i * 2 + 1];
 
-            bool isGreater = p[(i+2) % 3][1] > (m * p[(i+2) % 3][0] + b);
+            bool isGreater = p[((i+2) % 3) * 2 + 1] > (m * p[((i+2) % 3) * 2 + 0] + b);
             inTriangle = inTriangle && (isGreater ? p1y >= (m * p1x + b) : p1y <= (m * p1x + b));
         }
     }
@@ -95,26 +95,23 @@ bool pointInTri(float** p, float p1x, float p1y) {
     return inTriangle;
 }
 
-bool checkTesselatedTris(float** p1, float p1_num_verts, float** p2, float p2_num_verts) {
+bool checkTesselatedTris(float* p1, float p1_num_verts, float* p2, float p2_num_verts) {
     int v0 = 0;
     int v1 = 1;
     int v2 = p1_num_verts - 1;
     bool collision = false;
-    float** tempTri = new float* [3];
-    for(int i = 0; i < 3; ++i) {
-        tempTri[i] = new float[2];
-    }
+    float* tempTri = new float[6];
 
     while(v2-v1 >= 1) {
-        tempTri[0][0] = p1[v0][0];
-        tempTri[0][1] = p1[v0][1];
-        tempTri[1][0] = p1[v1][0];
-        tempTri[1][1] = p1[v1][1];
-        tempTri[2][0] = p1[v2][0];
-        tempTri[2][1] = p1[v2][1];
+        tempTri[0 * 2 + 0] = p1[v0 * 2 + 0];
+        tempTri[0 * 2 + 1] = p1[v0 * 2 + 1];
+        tempTri[1 * 2 + 0] = p1[v1 * 2 + 0];
+        tempTri[1 * 2 + 1] = p1[v1 * 2 + 1];
+        tempTri[2 * 2 + 0] = p1[v2 * 2 + 0];
+        tempTri[2 * 2 + 1] = p1[v2 * 2 + 1];
 
         for(int j = 0; j < p2_num_verts; ++j) {
-            collision = collision || pointInTri(tempTri, p2[j][0], p2[j][1]);
+            collision = collision || pointInTri(tempTri, p2[j * 2 + 0], p2[j * 2 + 1]);
         }
         v0 = v1;
         v1 = v2;
@@ -124,15 +121,15 @@ bool checkTesselatedTris(float** p1, float p1_num_verts, float** p2, float p2_nu
             break;
         }
 
-        tempTri[0][0] = p1[v0][0];
-        tempTri[0][1] = p1[v0][1];
-        tempTri[1][0] = p1[v1][0];
-        tempTri[1][1] = p1[v1][1];
-        tempTri[2][0] = p1[v2][0];
-        tempTri[2][1] = p1[v2][1];
+        tempTri[0 * 2 + 0] = p1[v0 * 2 + 0];
+        tempTri[0 * 2 + 1] = p1[v0 * 2 + 1];
+        tempTri[1 * 2 + 0] = p1[v1 * 2 + 0];
+        tempTri[1 * 2 + 1] = p1[v1 * 2 + 1];
+        tempTri[2 * 2 + 0] = p1[v2 * 2 + 0];
+        tempTri[2 * 2 + 1] = p1[v2 * 2 + 1];
 
         for(int j = 0; j < p2_num_verts; ++j) {
-            collision = collision || pointInTri(tempTri, p2[j][0], p2[j][1]);
+            collision = collision || pointInTri(tempTri, p2[j * 2 + 0], p2[j * 2 + 1]);
         }
 
         v0 = v1;
@@ -143,7 +140,7 @@ bool checkTesselatedTris(float** p1, float p1_num_verts, float** p2, float p2_nu
     return collision;
 }
 
-bool checkOverlap(float** p1, float p1_num_verts, float** p2, float p2_num_verts) {
+bool checkOverlap(float* p1, float p1_num_verts, float* p2, float p2_num_verts) {
     // TODO: Check if is line instead of polygon
 
     // Using custom algorithm I wrote, assuming polygon does not self-intersect and has non-identical vertices, and they're not a line either
@@ -156,19 +153,19 @@ bool checkOverlap(float** p1, float p1_num_verts, float** p2, float p2_num_verts
     return collision;
 }
 
-__device__ bool pointInTri_device(float** p, float p1x, float p1y) {
+__device__ bool pointInTri_device(float* p, float p1x, float p1y) {
     bool inTriangle = true;
     
     // If point not within triangle, then 3 eqs bounding triangle will not be true, so inTriangle will be false
     for(int i = 0; i < 3; ++i) {
-        if (p[(i+1) % 3][0] - p[i][0] == 0) {
-            bool isRight = p[(i+2) % 3][0] > p[i][0];
-            inTriangle = inTriangle && (isRight ? p1x >= p[i][0] : p1x <= p[i][0]); 
+        if (p[((i+1) % 3) * 2 + 0] - p[i * 2 + 0] == 0) {
+            bool isRight = p[((i+2) % 3 * 2) + 0] > p[i * 2 + 0];
+            inTriangle = inTriangle && (isRight ? p1x >= p[i * 2 + 0] : p1x <= p[i * 2 + 0]); 
         } else {
-            float m = (p[(i+1) % 3][1] - p[i][1])/(p[(i+1) % 3][0] - p[i][0]);
-            float b = -m * p[i][0] + p[i][1];
+            float m = (p[((i+1) % 3) * 2 + 1] - p[i * 2 + 1])/(p[((i+1) % 3) * 2 + 0] - p[i * 2 + 0]);
+            float b = -m * p[i * 2 + 0] + p[i * 2 + 1];
 
-            bool isGreater = p[(i+2) % 3][1] > (m * p[(i+2) % 3][0] + b);
+            bool isGreater = p[((i+2) % 3) * 2 + 1] > (m * p[((i+2) % 3) * 2 + 0] + b);
             inTriangle = inTriangle && (isGreater ? p1y >= (m * p1x + b) : p1y <= (m * p1x + b));
         }
     }
@@ -176,26 +173,23 @@ __device__ bool pointInTri_device(float** p, float p1x, float p1y) {
     return inTriangle;
 }
 
-__device__ bool checkTesselatedTris_device(float** p1, float p1_num_verts, float** p2, float p2_num_verts) {
+__device__ bool checkTesselatedTris_device(float* p1, float p1_num_verts, float* p2, float p2_num_verts) {
     int v0 = 0;
     int v1 = 1;
     int v2 = p1_num_verts - 1;
     bool collision = false;
-    float** tempTri = new float* [3];
-    for(int i = 0; i < 3; ++i) {
-        tempTri[i] = new float[2];
-    }
+    float* tempTri = new float[6];
 
     while(v2-v1 >= 1) {
-        tempTri[0][0] = p1[v0][0];
-        tempTri[0][1] = p1[v0][1];
-        tempTri[1][0] = p1[v1][0];
-        tempTri[1][1] = p1[v1][1];
-        tempTri[2][0] = p1[v2][0];
-        tempTri[2][1] = p1[v2][1];
+        tempTri[0 * 2 + 0] = p1[v0 * 2 + 0];
+        tempTri[0 * 2 + 1] = p1[v0 * 2 + 1];
+        tempTri[1 * 2 + 0] = p1[v1 * 2 + 0];
+        tempTri[1 * 2 + 1] = p1[v1 * 2 + 1];
+        tempTri[2 * 2 + 0] = p1[v2 * 2 + 0];
+        tempTri[2 * 2 + 1] = p1[v2 * 2 + 1];
 
         for(int j = 0; j < p2_num_verts; ++j) {
-            collision = collision || pointInTri_device(tempTri, p2[j][0], p2[j][1]);
+            collision = collision || pointInTri_device(tempTri, p2[j * 2 + 0], p2[j * 2 + 1]);
         }
         v0 = v1;
         v1 = v2;
@@ -205,15 +199,15 @@ __device__ bool checkTesselatedTris_device(float** p1, float p1_num_verts, float
             break;
         }
 
-        tempTri[0][0] = p1[v0][0];
-        tempTri[0][1] = p1[v0][1];
-        tempTri[1][0] = p1[v1][0];
-        tempTri[1][1] = p1[v1][1];
-        tempTri[2][0] = p1[v2][0];
-        tempTri[2][1] = p1[v2][1];
+        tempTri[0 * 2 + 0] = p1[v0 * 2 + 0];
+        tempTri[0 * 2 + 1] = p1[v0 * 2 + 1];
+        tempTri[1 * 2 + 0] = p1[v1 * 2 + 0];
+        tempTri[1 * 2 + 1] = p1[v1 * 2 + 1];
+        tempTri[2 * 2 + 0] = p1[v2 * 2 + 0];
+        tempTri[2 * 2 + 1] = p1[v2 * 2 + 1];
 
         for(int j = 0; j < p2_num_verts; ++j) {
-            collision = collision || pointInTri_device(tempTri, p2[j][0], p2[j][1]);
+            collision = collision || pointInTri_device(tempTri, p2[j * 2 + 0], p2[j * 2 + 1]);
         }
 
         v0 = v1;
@@ -224,7 +218,7 @@ __device__ bool checkTesselatedTris_device(float** p1, float p1_num_verts, float
     return collision;
 }
 
-__device__ bool checkOverlap_device(float** p1, float p1_num_verts, float** p2, float p2_num_verts) {
+__device__ bool checkOverlap_device(float* p1, float p1_num_verts, float* p2, float p2_num_verts) {
     // TODO: Check if is line instead of polygon
 
     // Using custom algorithm I wrote, assuming polygon does not self-intersect and has non-identical vertices, and they're not a line either
